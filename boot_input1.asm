6028b976641181b84495efbb67a7001bfef8a5af
kaandesu <kaandesu00@gmail.com>
2023-09-29 15:55:55 +0200

    basic input trial

--- boot_input1.asm
+++ boot_input1.asm
@@ -1,21 +1,37 @@
 [org 0x7c00]
 mov ah, 0x0e
+mov bx, myString
+
+printMyString:
+	mov al, [bx]
+	cmp al, 0
+	je exit
+	int 0x10
+	inc bx
+	jmp printMyString
+
+exit:
 
 
+mov al, 0x0A
+int 0x10
+int 0x10
+int 0x10
 
-char:
-	db 0
+mov al, 'K'
+int 0x10
+
 mov ah, 0
 int 0x16
 
-mov al, [char]
 
+mov ah, 0x0e
 int 0x10
+
 jmp $
 
 myString:
-	db "But the fool on the hill", 0
-
+	db "Welcome to kaanOS!",0
 
 times 510-($-$$) db 0
 dw 0xaa55
