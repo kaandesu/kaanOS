3b7fc5bff9c9070ce10188e3aba6258abf21c192
kaandesu <kaandesu00@gmail.com>
2023-09-29 23:24:46 +0200

    added: reading disk example with failure detection

--- boot_disk_hw.asm
+++ boot_disk_hw.asm
@@ -1,5 +1,5 @@
 [org 0x7c00]
-mov [BOOT_DISK], dl ; after boot drive number stored in dl (where booted) 
+mov [BOOT_DISK], dl 
 
 ; setting up the stack
 xor ax, ax
@@ -24,44 +24,15 @@ mov dl, [BOOT_DISK] ; drive number that we saved in a var
 ; so we did -> mov bx, 0x7e00
 int 0x13
 
-; Failure detection
-jnc disk_read_success ; checking if the carry flag is high
-mov bx, FAIL_MESSAGE_CARRY
-call printError
-disk_read_success:
-cmp al, 1
-je correct_sector_success
-mov bx, FAIL_MESSAGE_SEC_NUM
-call printError
-correct_sector_success:
-
 
 ; printing what is in the next sector
 mov ah, 0x0e
 mov al, [0x7e00]
 int 0x10
-mov al, 0x0D
-int 0x10
-
 
 jmp $  
-printError:
-	mov ah, 0x0e
-	printLoop:
-		mov al, [bx]
-		cmp al, 0
-		je exitPrint
-		int 0x10
-		inc bx
-		jmp printLoop
-	exitPrint:
-	mov al, 0x0D
-	ret
-	
-
 BOOT_DISK: db 0
-FAIL_MESSAGE_CARRY: db "Failure: carry flag (cf) is high!",0 
-FAIL_MESSAGE_SEC_NUM: db "Failure: wrong number of sectors to read!", 0
+
 
 ; padding 
 
--- boot_init.asm
+++ boot_init.asm
@@ -1,3 +1,5 @@
+[org 0x7c00]
+
 jmp $
 times 510-($-$$) db 0
 db 0x55, 0xaa
